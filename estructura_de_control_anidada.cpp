b7f89ff76048762889436b4f2da3bd29b02bb33c
GamersEC <marcusalexander1999@gmail.com>
2024-10-15 02:17:51 -0500

    cambios

--- estructura_de_control_anidada.cpp
+++ estructura_de_control_anidada.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main(){
 
+    string nombre;
+    int cedula;
+    char paralelo;
+    int carrera;
+    cout << "Ingrese su nombre:" << endl;
+    cin >> nombre;
+    cout << "Ingrese su numero de cedula:" << endl;
+    cin >> cedula;
+    cout << "Ingrese su paralelo:" << endl;
+    cin >> paralelo;
+    cout << "Seleccione su carrera" << endl;
+    cout << "1.Ingenieria en Ciencia de datos e IA" << endl;
+    cout << "2.Ingenieria Civil" << endl;
+    cout << "3.Ingenieria de Software" << endl;
+    cin >> carrera;
 
     return 0;
 }
