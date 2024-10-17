aa8aeeff863d6f3b8111fcc51bd5ebd04710358c
GamersEC <marcusalexander1999@gmail.com>
2024-10-17 13:03:40 -0500

    cambios

--- estructura_simple_compuesta.cpp
+++ estructura_simple_compuesta.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 
 int main(){
-    //Estructura de control de decisión simple
+    
     int edad;
     cout << "Ingrese su edad" << endl;
     cin >> edad;
+
+    //Estructura de control de decisión simple
     if (edad>=18)
     {
         cout << edad << " es mayor de edad" << endl; //Existe una sentencia por el verdadero
     }
 
-    else //Estructura de control de decisión compuesta
+    //Estructura de control de decisión compuesta
+    else
     {
         cout << edad << " es menor de edad" << endl; // Existe una sentencia por el falso
     }
