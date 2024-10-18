a0ea797f9f918b1a7f429970659ee38fb56a2a51
GamersEC <marcusalexander1999@gmail.com>
2024-10-17 21:27:03 -0500

    Nuevo ejercicio

--- ejercicio_restaurante.cpp
+++ ejercicio_restaurante.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int menu, precio, descuento, total;
+    int menu, precio;
     string dia;
 
     cout << "Seleccione un plato del menu" << endl;
@@ -11,23 +11,20 @@ int main(){
     cout << "3. Pescado: $20" << endl;
     cin >> menu;
 
-    cout << "Ingrese el dia de la semana" << endl;
-    cin >> dia;
-
     switch (menu)
     {
-    case '1':
+    case 1:
     {
         precio = 10;
         break;
 
     }
-    case '2':
+    case 2:
     {
         precio = 15;
         break;
     }
-    case '3':
+    case 3:
     {
         precio = 20;
         break;
@@ -35,26 +32,27 @@ int main(){
     default:
     {
         cout << "Opcion invalida" << endl;
+        return 1;
     }
     }
 
-    if (dia=="Lunes" || dia=="miercoles")
+    cout << "Ingrese el dia de la semana (use solo minusculas)" << endl;
+    cin >> dia;
+
+    if (dia=="lunes" || dia=="miercoles")
     {
-        descuento = precio*0.10;
-        total = precio - descuento;
+        precio = precio-(precio*0.1);
 
     } else if (dia=="viernes" && menu == 3)
     {
-        descuento = precio*0.15;
-        total = precio - descuento;
+        precio = precio-(precio*0.15);
+
     } else
     {
-        total = precio;
+        precio = precio;
     }
 
-    cout << "El precio a pagar es: " << total << endl;
+    cout << "El precio a pagar es: " << precio << endl;
 
-
-    cin.get();
     return 0;
     }
