6cfb2a338657f95d273eef415e1460d86ef4c084
GamersEC <marcusalexander1999@gmail.com>
2024-10-24 01:56:11 -0500

    Cambios ejercicio 1

--- Autonomo 1/autonomo1_1.cpp
+++ Autonomo 1/autonomo1_1.cpp
@@ -11,21 +11,21 @@ int main ()
 
     //Preguntas al usuario
     cout << "Bienvenido, completa los siguientes datos" << endl;
-    cout << "Ingresa la inicial de tu nombre" << endl;
+    cout << "Ingresa la inicial de tu nombre: ";
     cin >> inicial;
-    cout << "Ingresa tu primer nombre" << endl;
+    cout << "Ingresa tu primer nombre: ";
     cin >> nombre;
-    cout << "Ingresa tu primer apellido" << endl;
+    cout << "Ingresa tu primer apellido: ";
     cin >> apellido; 
-    cout << "Ingresa tu edad" << endl;
+    cout << "Ingresa tu edad: ";
     cin >> edad;
-    cout << "Ingrese su altura en metros" << endl;
+    cout << "Ingrese su altura en metros: ";
     cin >> altura;
-    cout << "Eres estudiante" << endl;
-    cout << "Si/No" << endl;
+    cout << "Eres estudiante (Si/No): ";
     cin >> estudiante;
 
     //Salida en terminal
+    cout << "Estos son los datos que ingresaste" << endl;
     cout << "Nombre: " << nombre << endl;
     cout << "Inicial: " << inicial << endl;
     cout << "Apellido: " << apellido << endl;
