9fe3fede71126f1b6d50d4a94cd91e0a57d3ba75
GamersEC <marcusalexander1999@gmail.com>
2024-11-12 21:40:32 -0500

    cambios

--- Ejercicios/recursividad4.cpp
+++ Ejercicios/recursividad4.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 // Serie de potencias: solicita por teclado base y exponente, y calcula la suma de los primeros n términos
-// Ejemplo: para base = 2 y exponente = 3, la serie sería 2^3 + 2^6 + 2^9 + ...
+// Ejemplo: para base inicial = 2 y exponente = 3, la serie sería 2^3 + 8^3 + 27^3 + ...
 
 long long seriepotencia(long long base, int exponente, int limite) {
     if (limite == 0) {
-        return 0;
+        return 0; // Caso base: cuando ya no hay más términos
     } else {
-        long long term = pow(base, exponente * limite);
-        return term + seriepotencia(base, exponente, limite - 1);
+        long long term = pow(base, exponente); // Calcula el término actual de la serie
+        return term + seriepotencia(base + 1, exponente, limite - 1); // Incrementa base, acumula el término actual y llama recursivamente
     }
 }
 
@@ -18,7 +18,7 @@ int main() {
     long long base;
     int exponente, limite;
     
-    cout << "Ingrese la base: ";
+    cout << "Ingrese la base inicial: ";
     cin >> base;
     cout << "Ingrese el exponente: ";
     cin >> exponente;
