d4d034b3c399a01aade5ed4c0e0540fb86186ba8
GamersEC <marcusalexander1999@gmail.com>
2024-11-27 11:17:17 -0500

    Cambios

--- Parcial1.cpp
+++ Parcial1.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 using namespace std;
 
-//Funcion booleana
+// Función booleana
 bool digito(char c) {
     return c >= '0' && c <= '9';
 }
 
-//Funcion recursiva para calcular serie
-void serie(int* serie, int indice, int cantidad) {
-    if (indice == cantidad) {
-        serie [indice] = (serie[indice -1] * serie [indice - 1]) - 1;
+// Función recursiva para calcular serie
+void generarSerie(int* serie, int indice, int cantidad) {
+    if (indice >= cantidad) {
+        return; // Caso base
     }
-    serie(serie, indice + 1, cantidad);
+    serie[indice] = (serie[indice - 1] * serie[indice - 1]) - 1;
+    generarSerie(serie, indice + 1, cantidad); // Llamada recursiva
 }
 
-//Funcion contar impares
+// Función contar impares
 int impares(int inicio, int fin) {
     int contador = 0;
     for (int i = inicio; i <= fin; i++) {
@@ -25,87 +26,102 @@ int impares(int inicio, int fin) {
     return contador;
 }
 
-//Funciones sobrecargadas
-
-//Funcion para el area del circulo
-double area(double radio) {
+// Función para el área del círculo
+double areaCirculo(double radio) {
     return 3.14 * radio * radio;
 }
 
-//Funcion para el area del triangulo
-double area(double base, double altura) {
-    return (base * altura)/2;
+// Función para el área del triángulo
+double areaTriangulo(double base, double altura) {
+    return (base * altura) / 2;
 }
 
-//Funcion principal
+// Función principal
 int main() {
-char opc, caracter;
+    char opc, caracter;
     int termino1, terminostotal, inicio, fin, totalimpares, menuarea;
     double radio, base, altura;
-    cout << "Seleccione la opcion que desea realizar" << endl;
-    cout << "a. Funcion booleana" << endl;
-    cout << "b. Serie Matematica" << endl;
-    cout << "c. Contar numeros impares" << endl;
-    cout << "d. Area de un circulo o rectangulo" << endl;
+
+    cout << "Seleccione la opción que desea realizar" << endl;
+    cout << "a. Función booleana" << endl;
+    cout << "b. Serie Matemática" << endl;
+    cout << "c. Contar números impares" << endl;
+    cout << "d. Área de un círculo o triángulo" << endl;
     cin >> opc;
 
     switch (opc) {
         case 'a': {
-            cout << "Ingrese un caracter: ";
+            cout << "Ingrese un carácter: ";
             cin >> caracter;
             if (digito(caracter)) {
-                cout << caracter << " es un digito de 0-9" << endl;
-            }else {
-                cout << caracter << " no es un digito de 0-9" << endl;
+                cout << caracter << " es un dígito de 0-9" << endl;
+            } else {
+                cout << caracter << " no es un dígito de 0-9" << endl;
             }
-        }break;
+        } break;
 
         case 'b': {
-            cout << "Ingrese el primer termino: ";
+            cout << "Ingrese el primer término: ";
             cin >> termino1;
-            cout << "Ingrese la cantidad de terminos que desea generar: ";
+            cout << "Ingrese la cantidad de términos que desea generar: ";
             cin >> terminostotal;
 
-            int serie [terminostotal];
+            if (terminostotal <= 0) {
+                cout << "La cantidad de términos debe ser mayor que 0." << endl;
+                break;
+            }
+
+            int* serie = new int[terminostotal]; // Uso de memoria dinámica
             serie[0] = termino1;
-            serie(serie, 1, terminostotal);
+            generarSerie(serie, 1, terminostotal);
 
             cout << "La serie es: ";
             for (int i = 0; i < terminostotal; i++) {
                 cout << serie[i] << " ";
             }
             cout << endl;
-        }break;
+
+            delete[] serie; // Liberar memoria
+        } break;
 
         case 'c': {
-            cout << "Ingrese el numero inicial del rango: ";
+            cout << "Ingrese el número inicial del rango: ";
             cin >> inicio;
-            cout << "Ingrese el numero final del rango: ";
+            cout << "Ingrese el número final del rango: ";
             cin >> fin;
+
+            if (inicio > fin) {
+                cout << "El número inicial debe ser menor o igual al final." << endl;
+                break;
+            }
+
             totalimpares = impares(inicio, fin);
-            cout << "La cantidad de numeros impares en el rango ingresado es: " << totalimpares << endl;
-        }break;
+            cout << "La cantidad de números impares en el rango ingresado es: " << totalimpares << endl;
+        } break;
 
         case 'd': {
-            cout << "Selecion el Area que desea calcular" << endl;
-            cout << "1. Calcular Area de Circulo" << endl;
-            cout << "2. Calcular el Area del triangulo" << endl;
+            cout << "Seleccione el área que desea calcular" << endl;
+            cout << "1. Calcular área del círculo" << endl;
+            cout << "2. Calcular el área del triángulo" << endl;
             cin >> menuarea;
 
             if (menuarea == 1) {
-                cout << "Ingrese el radio del circulo: ";
+                cout << "Ingrese el radio del círculo: ";
                 cin >> radio;
-                cout << "El area del circulo es: " << area(radio) << endl;
+                cout << "El área del círculo es: " << areaCirculo(radio) << endl;
             } else if (menuarea == 2) {
-                cout << "Ingrese la base del triangulo: ";
+                cout << "Ingrese la base del triángulo: ";
                 cin >> base;
-                cout << "Ingrese la altura del triangulo: ";
+                cout << "Ingrese la altura del triángulo: ";
                 cin >> altura;
-                cout << "El area del triangulo es: " << area(base, altura) << endl;
+                cout << "El área del triángulo es: " << areaTriangulo(base, altura) << endl;
             } else {
-                cout << "Opcion no valida..." << endl;
+                cout << "Opción no válida..." << endl;
             }
-        }
+        } break;
+
+        default:
+            cout << "Opción no válida." << endl;
     }
 
     return 0;
