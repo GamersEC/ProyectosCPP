6576570e43505c5c651af0ad4083ace7b5d916ec
GamersEC <marcusalexander1999@gmail.com>
2024-12-03 20:51:50 -0500

    Cambios

---
+++ Ejercicios/ejericiovectores1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std
+
+int main(){
+
+    int numeros[5] = {5, 6, 4, 60, 7};
+    for (int i=0; i<5; i++)
+    {
+        cout << "Elemento: " << i << " : " << numeros[i];
+    }
+
+    return 0;
+}
